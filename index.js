0c24114b148b335b28a2ad8476bbebb426ffefef
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-20 13:51:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
+var destructivelyAppendKitten = function(name){
+  return kittens.push(name)
+}
+
+var destructivelyPrependKitten = function(name){
+  return kitten.unshift(name)
+}
